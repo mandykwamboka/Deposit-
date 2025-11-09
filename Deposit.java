4f5ec22fd4abf9cabc10d72537f054bdeb029e7f
mandy kwamboka <163193071+mandykwamboka@users.noreply.github.com>
2025-11-09 22:48:58 +0300

    Refactor deposit method in Deposit class
    Refactored deposit method to include balance as a parameter.

--- Deposit.java
+++ Deposit.java
@@ -1,29 +1,16 @@
-import java.util.Scanner;
-
-   public class Deposit {
+public class Deposit {
    
-    private double balance; 
-
-    // Set the initial balance
-    public void setBalance(double initialBalance) {
-        balance = initialBalance;
-    }
-
-    // Add money to balance
-    public void deposit(double depositamount) {
+  // Add money to balance
+    public double deposit(double depositamount, double balance) {
         if (depositamount > 0) {
             balance = depositamount + balance;
+            
             System.out.println("Deposited: " + depositamount);
             System.out.println("Total balance: " + balance);
+
         } else {
             System.out.println("You cannot deposit less than 0");
         }
-    }
-
-    // Get the current balance
-    public double getBalance() {
         return balance;
     }
  }
-
-   
